398263bba98da4b19348cfa2a6db38307a403b18
redWare() <redgamer295k@gmail.com>
2026-03-19 20:42:29 +0000

    No Idea what I'm doing

---
+++ functions.h
@@ -0,0 +1,12 @@
+// declaration of functions
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#endif // FUNCTIONS_H
+
+//prompt the user for two numbers
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(int a, int b);
+int factorial(int n);
--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include "functions.h"
 using namespace std;
 
 int main()
 {
-    std::cout << "Goodbye Jojo";
-    return 0 ;
+    //prompt the user for two numbers
+    int num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << endl << "Enter the second number: ";
+    cin >> num2;
+
+    //Perform arithmetic and display results!
+    int sum = add(num1, num2);
+    cout << "Sum: " << sum << endl; 
+    int difference = subtract(num1, num2);
+    cout << "Difference: " << difference << endl; 
+    int product = multiply(num1, num2);
+    cout << "Sum: " << product << endl; 
+    int quotient = divide(num1, num2);
+    cout << "Sum: " << quotient << endl; 
+    int factor = factorial(num1);
+    cout << "Factorial: " << factorial << endl; 
+
+    return 0;
 }
