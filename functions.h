260267d8ae8713f6e66879c4faf15a0390b197f3
redWare() <redgamer295k@gmail.com>
2026-03-30 22:41:22 +0000

    Defined the functions in a custom library

--- functions.h
+++ functions.h
@@ -1,12 +1,25 @@
-// declaration of functions
+// declaration of functions used in the project
 
 #ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+    #define FUNCTIONS_H
 #endif // FUNCTIONS_H
 
+//declaring functions
+
 //prompt the user for two numbers
-int add(int a, int b);
-int subtract(int a, int b);
-int multiply(int a, int b);
-double divide(int a, int b);
-int factorial(int n);
+int add(int a, int b){
+    return a + b;
+}
+int subtract(int a, int b){
+    return a - b;
+}
+int multiply(int a, int b){
+    return a * b;
+}
+double divide(int a, int b){
+    return a / b;
+}
+int factorial(int n){
+        return 1;
+    return n * factorial(n-1);
+}
